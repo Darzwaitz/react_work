baf9365cecdedbafe4422bf51ae607dd10e10f2a
darzwaitz <darzwaitz@yahoo.com>
2023-05-11 17:50:15 +0100

    add temp footer comp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 // snippetz
-// rafce -> functional comp
+// rafce -> functional comp, imports react
+// rce -> export default class react
 
 import "./App.css";
 import Header from "./Header";
 import Content from "./Content";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Content />
+      <Footer />
     </div>
   );
 }
--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Content = () => {
   const compTest = () => {
     const text = "is working";
