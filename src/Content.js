23932fb935ddb9b149937653dc4f7d68087c48a6
darzwaitz <darzwaitz@yahoo.com>
2023-05-15 18:46:28 +0100

    experimenting with functions in react component

--- src/Content.js
+++ src/Content.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 const Content = () => {
+  const [name, setName] = useState("Darz");
   const compTest = () => {
     const text = "is working";
     return text;
   };
+  const currentData = "text changed";
+
+  let handleClick = (e) => {
+    // let handleClickBool = true;
+    e.target.innerText = currentData;
+    console.log(`name is: ${name},setName is: ${setName}`);
+  };
   return (
     <main>
       <p>Test {compTest()}</p>
+      <p>Hello {name}</p>
+      <button onClick={handleClick}>temp text</button>
     </main>
   );
 };
