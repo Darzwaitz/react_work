9290318773519a19721688107de14a6013164c1c
darzwaitz <darzwaitz@yahoo.com>
2023-05-18 13:50:39 +0100

    adding state change to localstorage for checkbox

--- src/Content.js
+++ src/Content.js
@@ -25,11 +25,14 @@ const Content = () => {
     },
   ]);
 
+  // localStorage.getItem("shoppinglist");
+
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
     setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
   };
 
   return (
@@ -43,7 +46,12 @@ const Content = () => {
                 onChange={() => handleCheck(item.id)}
                 checked={item.checked}
               />
-              <label>{item.item}</label>
+              <label
+                style={item.checked ? { textDecoration: "line-through" } : null}
+                onDoubleClick={() => handleCheck(item.id)}
+              >
+                {item.item}
+              </label>
               <FaTrashAlt role="button" tabIndex="0" />
             </li>
           );
