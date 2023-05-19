fd0af1e9787de56169252accf0ac6b9dc38ed1aa
darzwaitz <darzwaitz@yahoo.com>
2023-05-19 13:49:44 +0100

    refactoring, to pass in data with props

--- src/Content.js
+++ src/Content.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { FaTrashAlt } from "react-icons/fa";
 
 // useState accepts an initial state and returns two values:
@@ -6,40 +6,8 @@ import { FaTrashAlt } from "react-icons/fa";
 //     The current state.
 //     A function that updates the state.
 
-const Content = () => {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: "Item 1",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3",
-    },
-  ]);
-
+const Content = ({ items, handleCheck, handleDelete }) => {
   // localStorage.getItem("shoppinglist");
-
-  const handleCheck = (id) => {
-    const listItems = items.map((item) =>
-      item.id === id ? { ...item, checked: !item.checked } : item
-    );
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-  };
-
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-  };
 
   return (
     <main>
