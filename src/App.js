9d1bf3aa84cc6d5f414fcd7cd57ef0861992818e
darzwaitz <darzwaitz@yahoo.com>
2023-05-23 14:58:20 +0100

    controlled components - i.e. form inputs - Part 1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
 import { useState } from "react";
+import AddItem from "./AddItem";
 
 function App() {
   const [items, setItems] = useState([
@@ -25,6 +26,8 @@ function App() {
     },
   ]);
 
+  const [newItem, setNewItem] = useState("");
+
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
@@ -39,9 +42,18 @@ function App() {
     localStorage.setItem("shoppinglist", JSON.stringify(listItems));
   };
 
+  const handleSubmit = (e) => {
+    console.log("submitted");
+  };
+
   return (
     <div className="App">
       <Header title="Groceries" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
       <Content
         items={items}
         handleCheck={handleCheck}
