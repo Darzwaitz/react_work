d25b33612743db1079d5e32c6afca6011335a2b2
darzwaitz <darzwaitz@yahoo.com>
2023-05-31 23:05:33 +0100

    using json-server with npx & using fetch api within useeffect hook

--- src/App.js
+++ src/App.js
@@ -9,16 +9,20 @@ import Footer from "./Footer";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("shoppinglist")) || []
-  );
+  const API_URL = "http://localhost:3500";
+
+  const [items, setItems] = useState([]);
 
   const [newItem, setNewItem] = useState("");
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    localStorage.setItem("shoppinglist", JSON.stringify(items));
-  }, [items]);
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+      } catch (error) {}
+    };
+  }, []);
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
