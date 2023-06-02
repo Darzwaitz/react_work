2c36c3ce699ec3863f2c839f55d3d52ae218425e
darzwaitz <darzwaitz@yahoo.com>
2023-06-02 18:58:31 +0100

    added Home and About comps

--- film-browser/src/App.js
+++ film-browser/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Navbar from "./components/navigation/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
+      <About />
       <h1>First comp.</h1>
     </div>
   );
