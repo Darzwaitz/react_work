bf118ac943f5d9ef671d36894caedec9665114b0
darzwaitz <darzwaitz@yahoo.com>
2023-06-02 19:29:16 +0100

    downgraded react-router-dom to previous version & set up first 2 routes

--- film-browser/src/App.js
+++ film-browser/src/App.js
@@ -2,14 +2,18 @@ import "./App.css";
 import Navbar from "./components/navigation/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Home />
-      <About />
-      <h1>First comp.</h1>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about" component={About} />
+      </Switch>
     </div>
   );
 }
