999ea58c4eac737c94ffcb97de070e6c5a946b20
darzwaitz <darzwaitz@yahoo.com>
2023-06-06 19:26:34 +0100

    added simple props and some content to comps

--- film-browser/src/components/pages/About.js
+++ film-browser/src/components/pages/About.js
@@ -1,10 +1,22 @@
 import Hero from "../Hero";
-const About = () => {
+const About = ({ text }) => {
   return (
-    <div>
-      <Hero />
-      About
-    </div>
+    <main>
+      <Hero text="About Us" />
+
+      <section className="container">
+        <div className="row">
+          <div className="col-lg-8 offset-lg-2 my-5">
+            <p className="lead">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis,
+              mollitia fugiat quaerat minima magni possimus dolorem eveniet
+              dicta vero aperiam a eum alias ducimus, ipsam perferendis modi
+              exercitationem at placeat!
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 };
 
--- film-browser/src/components/pages/Home.js
+++ film-browser/src/components/pages/Home.js
@@ -2,10 +2,21 @@ import Hero from "../Hero";
 
 const Home = () => {
   return (
-    <div>
-      <Hero />
-      Home
-    </div>
+    <main>
+      <Hero text="Hero comp" />
+      <section className="container">
+        <div className="row">
+          <div className="col-lg-8 offset-lg-2 my-5">
+            <p className="lead">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus, quo aliquam, quis accusantium magni reprehenderit
+              deleniti, laudantium pariatur sunt exercitationem voluptatum eum
+              provident! Explicabo.
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 };
 
