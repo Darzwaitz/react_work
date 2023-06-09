3c4ce8e1e953fa148019260965b0efe5c3089dcd
darzwaitz <darzwaitz@yahoo.com>
2023-06-09 19:17:54 +0100

    updating via an API

--- film-browser/src/App.js
+++ film-browser/src/App.js
@@ -4,12 +4,15 @@ import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import SearchView from "./components/SearchView";
 import { Switch, Route } from "react-router-dom";
-import { useState } from "react";
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [searchResults, setSearchResults] = useState([]);
+  // const [searchResults, setSearchResults] = useState([]);
+  const [searchResults] = useState([]);
   const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {}, [searchText]);
+
   return (
     <div>
       <Navbar searchText={searchText} setSearchText={setSearchText} />
